1b997ec264413a533ab5778b110c3e6d3a3f5f91
“hxc338” <hxc338@student.bham.ac.uk>
2025-06-05 16:21:38 +0100

    Set up folders, views layouts to understand routes, understand how to render ejs and starting css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+// npm run dev to run the app
 import express from "express";
 import dotenv from "dotenv";
 import expressEjsLayouts from "express-ejs-layouts";
@@ -12,14 +12,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000; // allows your app to use the port the hosting service provides.
 
-app.use(express.static("public")); // holds css, js, img
+app.use(express.static("public")); // public folder will hold css, js, img
+// don't need to use ../../ as it know it's all in public folder
 
-// Templating Engine
+//* Templating Engine
 app.use(expressEjsLayouts); // tells Express to use the express-ejs-layouts middleware
 // allows you to have one main layout (like a template with a header, footer, etc.) and insert different pages (views) into it.
 app.set("layout", "./layouts/main"); // tells Express EJS Layouts where your main layout file is located.
 // "Use the ./layouts/main.ejs file as the base layout for all views."
 app.set("view engine", "ejs") //tells Express that you're using EJS (Embedded JavaScript) as the view engine.
+
+//! We don't want all our routes in app.js so put them in main.js
 
 app.use("/", mainRoutes);
 //  tells your Express app to use the routes defined in mainRoutes (your main.js file) for any requests that start with /
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,27 @@
 // import express from "express"
 // const app = express();
 
-// We have multiple routes - this will have all the main routes like homepage, contact, about etc
+//! We have multiple routes - this will have all the main routes like homepage, contact, about etc
 
 import { Router } from "express"; // importing the Router function from Express.
+
 const router = Router() // holds all the routes for this module (main.js)
 
 // Routes
 router.get("", (req, res) => {
-    res.send("Hello World")
+
+    // This renders/pass ejs data such as rendering to layouts
+    // Created an object with title & description so we can pass it to a page 
+    const locals = { 
+        title: "Node.js blog",
+        description: "A simple blog made with node.js, express and MongoDb"
+    }
+    // Pass "locals" to a page and render it, if passing more objects put it in {}
+    res.render("index", { locals }) // we are rendering the home page from ./views/index 
+});
+
+router.get("/about", (req, res) => {
+    res.render("about")  
 });
 
 export default router // exports the router so you can use it in your main app file
